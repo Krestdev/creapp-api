ed00e50412f899bffd8981d3cfb781a94e28d0bf
wintercode <kenfackjordanjunior@gmail.com>
2026-03-17 15:25:28 +0100

    picture limit

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 15 * 1024 * 1024 }, // 5MB limit
   fileFilter: (req, file, cb) => {
     const allowedTypes = [
       "image/jpg",
