25ab3c616087794efef9255499ce673d9255d559
wintercode <kenfackjordanjunior@gmail.com>
2026-06-01 17:58:40 +0100

    feat: initialize Prisma schema and define service head relations

---
+++ prisma/migrations/20260601103604_services_head/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Provider" ALTER COLUMN "expireAtacf" SET DEFAULT now() + interval '1 year',
+ALTER COLUMN "expireAtbanck_attestation" SET DEFAULT now() + interval '1 year',
+ALTER COLUMN "expireAtcommerce_registre" SET DEFAULT now() + interval '1 year',
+ALTER COLUMN "expireAtplan_localisation" SET DEFAULT now() + interval '1 year';
