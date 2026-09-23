16e7a5841d6e4aa8393a167c0ae3288e8a1a5f0b
wintercode <kenfackjordanjunior@gmail.com>
2026-09-23 14:59:13 +0100

    feat: implement TransactionService for handling financial operations and bank balances

--- src/modules/transaction/transaction.Service.ts
+++ src/modules/transaction/transaction.Service.ts
@@ -1075,7 +1075,6 @@ export class TransactionService {
         to: true,
         method: true,
         payementappro: true,
-        payementappro: true,
         signers: {
           include: { user: true },
         },
